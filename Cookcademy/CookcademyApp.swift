af6989efeea1272bb721189968b9901d8b0e8e88
Juan Rozo <juan.rozo@globant.com>
2025-02-13 17:02:53 -0500

    Initial Commit

---
+++ Cookcademy/CookcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookcademyApp.swift
+//  Cookcademy
+//
+//  Created by Juan Esteban Rozo Mendez on 13/02/25.
+//
+
+import SwiftUI
+
+@main
+struct CookcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
